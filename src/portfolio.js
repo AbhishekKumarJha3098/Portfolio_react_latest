cde3e8c45161afb2e7623e6d31602ec16eb39b5e
Abhishek Kumar Jha <87441996+AbhishekKumarJha3098@users.noreply.github.com>
2022-02-18 20:56:05 +0530

    Add files via upload

---
+++ src/portfolio.js
@@ -0,0 +1,132 @@
+//I Would Highly Recommend You To Customize The Theme According To Your Taste.
+export const globalStyles = {
+
+    "--grad": "linear-gradient(315deg,#48a9fe,#00cdac)", // Use gradients only
+    // "--grad": "linear-gradient(315deg, #48a9fe 0%, #734ae8 74%)", // Use gradients only
+    // "--grad": "linear-gradient(315deg, #06bcfb 0%, #4884ee 74%)",
+    "--txtcolor": "#272341",
+    "--p_color": "#00cdac",
+    "--s_color": "#fff"
+}
+
+// Navigation Bar Name
+export const navbar_name = "Abhishek"
+
+// Main Page
+export const main = {
+    name: "Abhishek Jha",
+    doamin: "Full Stack Web Developer",
+    resume: "https://drive.google.com/file/d/1MaB5IaipAv039LNRiNL53cfiZlGu6yJ4/view?usp=sharing"
+}
+
+// AboutMe
+export const aboutme = {
+    imgLink: "https://pbs.twimg.com/media/FLuc7e7aUAcpyDm?format=jpg&name=small",
+    p1: "Hey,My name is Abhishek Jha. A passionate individual who always thrive to work on end to end products",
+    p2: "I'm a Tech Enthusiast who enjoys building awesome website that solves real world problems.",
+    p3: "Innovative, task-driven professional in web-designing, proficent in developing databases, user interfaces, writing & testing Codes, and implementing the featured based on the feedback."
+}
+
+// Skills
+export const whatido = {
+    title: "Full Stack Development",
+
+    details: ["Building responsive website using React",
+        "Creating backend application in Node, Express",
+        "Building Simple RESTful CRUD APIs",
+        "Knowledge of Object-Oriented programming"],
+    logos: [{
+        name: "HTML5",
+        iconifyClassName: "vscode-icons:file-type-html", // PS : Use iconify class name
+        id: "1"
+    },
+    {
+        name: "CSS",
+        iconifyClassName: "vscode-icons:file-type-css",
+        id: "2"
+    },
+    {
+        name: "Tailwind",
+        iconifyClassName: "logos:tailwindcss-icon",
+        id: "3"
+    },
+    {
+        name: "JavaScript",
+        iconifyClassName: "logos:javascript",
+        id: "4"
+    },
+    {
+        name: "ReactJS",
+        iconifyClassName: "vscode-icons:file-type-reactjs",
+        id: "5"
+    },
+    {
+        name: "Redux",
+        iconifyClassName: "logos:redux",
+        id: "6"
+    },
+    {
+        name: "NodeJS",
+        iconifyClassName: "logos:nodejs",
+        id: "7"
+    },
+    {
+        name: "MongoDB",
+        iconifyClassName: "logos:mongodb",
+        id: "8"
+    },
+    {
+        name: "NPM",
+        iconifyClassName: "logos:npm-icon",
+        id: "9"
+    },
+    {
+        name: "VSCode",
+        iconifyClassName: "vscode-icons:file-type-vscode",
+        id: "10"
+    },
+    {
+        name: "PostMan",
+        iconifyClassName: "logos:postman-icon",
+        id: "11"
+    },
+    {
+        name: "Git",
+        iconifyClassName: "bi:git",
+        id: "12"
+    },
+    {
+        name: "Vercel",
+        iconifyClassName: "logos:vercel",
+        id: "13"
+    }
+    ]
+}
+
+// Project Details
+export const details = [{
+    id: 1,
+    title: "Clone of Practo.in",
+    desc: "An informative website that provides online Doctor Consultant, Booking Appointments, Purchasing Medicines, Delivery of Varieties of Medical produts and  many more.  Tech Stack - HTML, CSS, Javascript, Bootstrap.",
+    img: "https://pbs.twimg.com/media/FLuT0LzaIAcyZUq?format=jpg&name=900x900",
+    github: "https://github.com/AbhishekKumarJha3098/Practo_clone",
+    link: "https://youtu.be/1x73l7V_Egk",
+    deploy:"https://karu555.github.io/practoClone/"
+    
+},
+{
+    id: 2,
+    title: "Clone of Porter.in",
+    desc: "A web application that transport goods, shift houses, local parcel,a courier delivery, and Hiring of Part-timer Delivery guy. Tech Stack - HTML, CSS, Javascript, Bootstrap, ExpressJS, Mongodb.",
+    img: "https://pbs.twimg.com/media/FLuVfr6aMAMP8tF?format=jpg&name=900x900",
+    link: "https://youtu.be/5IRJbiz0C9I",
+    github: "https://github.com/AbhishekKumarJha3098/Porter-Clone",
+    deploy:"https://porterclone.herokuapp.com/"
+}
+]
+
+// Social Media Links
+export const links = ["https://www.linkedin.com/in/abhishek-jha-2503a0179/",
+    "https://github.com/AbhishekKumarJha3098",
+    "https://www.instagram.com/abhishekjha_3098/",
+    "https://twitter.com/abhishekjha3098"]
